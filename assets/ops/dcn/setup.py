515fe0c49a270e5479d7130b84a2236fb60593cb
giangbang <44130635+giangbang@users.noreply.github.com>
2021-06-26 03:21:57 +0700

    Update setup.py

--- assets/ops/dcn/setup.py
+++ assets/ops/dcn/setup.py
@@ -3,15 +3,24 @@ from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 cxx_args = ['-std=c++14']
 
+nvcc_args = [
+    '-gencode', 'arch=compute_50,code=sm_50',
+    '-gencode', 'arch=compute_52,code=sm_52',
+    '-gencode', 'arch=compute_60,code=sm_60',
+    '-gencode', 'arch=compute_61,code=sm_61',
+    '-gencode', 'arch=compute_70,code=sm_70',
+    '-gencode', 'arch=compute_70,code=compute_70'
+]
+
 setup(
     name='deform_conv',
     ext_modules=[
         CUDAExtension('deform_conv_cuda', [
             'src/deform_conv_cuda.cpp',
             'src/deform_conv_cuda_kernel.cu',
-        ], extra_compile_args={'cxx': cxx_args}), 
+        ], extra_compile_args={'cxx': cxx_args, 'nvcc': nvcc_args}), 
         CUDAExtension('deform_pool_cuda', [
             'src/deform_pool_cuda.cpp', 'src/deform_pool_cuda_kernel.cu'
-        ], extra_compile_args={'cxx': cxx_args}),
+        ], extra_compile_args={'cxx': cxx_args, 'nvcc': nvcc_args}),
     ],
     cmdclass={'build_ext': BuildExtension})
