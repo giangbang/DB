3a698589da0d2e8d6cf27a241a84805163e615d3
giangbang <44130635+giangbang@users.noreply.github.com>
2021-06-26 03:16:14 +0700

    Update setup.py

--- assets/ops/dcn/setup.py
+++ assets/ops/dcn/setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
+cxx_args = ['-std=c++14']
+
 setup(
     name='deform_conv',
     ext_modules=[
         CUDAExtension('deform_conv_cuda', [
             'src/deform_conv_cuda.cpp',
             'src/deform_conv_cuda_kernel.cu',
-        ]),
+        ], extra_compile_args={'cxx': cxx_args}), 
         CUDAExtension('deform_pool_cuda', [
             'src/deform_pool_cuda.cpp', 'src/deform_pool_cuda_kernel.cu'
-        ]),
+        ], extra_compile_args={'cxx': cxx_args}),
     ],
     cmdclass={'build_ext': BuildExtension})
